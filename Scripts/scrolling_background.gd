144f2734c7cbba5c054129af5b25e379441011ec
Simon Edmunds <sjebkc@umsystem.edu>
2025-03-02 00:31:34 -0600

    Fixed scrolling background bug

--- Scripts/scrolling_background.gd
+++ Scripts/scrolling_background.gd
@@ -7,19 +7,15 @@ var end_pos
 
 func _ready():
 	pass
-	
+
 func start_scroll():
 	var tween = get_tree().create_tween()
-	#var sprite = $Sprite2D
-	#start_pos = sprite.position
-	#end_pos = start_pos + Vector2(0, sprite.texture.get_height())  # Move by full image height
-
 	tween.tween_property($Sprite2D, "position", end_pos, scroll_time).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_LINEAR)
-
 
 func _on_floating_world_game_started(disable_inputs: bool) -> void:
 	if disable_inputs:
 		start_pos = $Sprite2D.position
-		end_pos = start_pos + Vector2(0, $Sprite2D.texture.get_height())
+		var image_height = $Sprite2D.texture.get_height() * $Sprite2D.scale.y - 600
+		end_pos = start_pos + Vector2(0, image_height)
 	else:
 		start_scroll()
