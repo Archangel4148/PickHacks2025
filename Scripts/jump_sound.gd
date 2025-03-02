4b68a5619714a06acb70ca2f1bfd35573e316726
Simon Edmunds <sjebkc@umsystem.edu>
2025-03-01 23:06:06 -0600

    Working on sound effects

--- Scripts/jump_sound.gd
+++ Scripts/jump_sound.gd
@@ -1,5 +1,8 @@
 extends AudioStreamPlayer2D
 
+@onready var jump_sound = preload("res://sound effects/grunts/grunt1.wav")  # Preload the sound
+
 func _on_player_player_jumped(is_double_jump: bool) -> void:
 	if !is_double_jump:
-		play()
+		stream = jump_sound
+		play(0.2)
