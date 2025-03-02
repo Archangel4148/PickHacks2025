8cb6b8b1be287098b197f84f284b6f34057ae377
Simon Edmunds <sjebkc@umsystem.edu>
2025-03-02 00:02:33 -0600

    Fixed size/hitbox of slime

--- Scripts/jump_sound.gd
+++ Scripts/jump_sound.gd
@@ -1,8 +1,8 @@
-extends AudioStreamPlayer2D
+extends AudioStreamPlayer
 
-@onready var jump_sound = preload("res://sound effects/grunts/grunt1.wav")  # Preload the sound
+@export var jump_sounds: Array[AudioStream] = []  # Assign multiple jump sounds in the Inspector
 
 func _on_player_player_jumped(is_double_jump: bool) -> void:
-	if !is_double_jump:
-		stream = jump_sound
-		play(0.2)
+	if !is_double_jump and jump_sounds.size() > 0:
+		stream = jump_sounds[randi() % jump_sounds.size()]  # Pick a random sound
+		play(0.03)
